9353f4255cf0a49d2e0d431300b0a505684627ae
oussamahannachi <oussama.hannachi.1@esprit.tn>
2020-03-14 21:37:18 +0100

    AllInterfaces

--- TimeSheet-ejb/src/main/java/interfaces/EntrepriseServiceRemote.java
+++ TimeSheet-ejb/src/main/java/interfaces/EntrepriseServiceRemote.java
@@ -7,10 +7,8 @@ import entities.*;
 
 @Remote
 public interface EntrepriseServiceRemote {
-
 	public int ajouterEntreprise(Entreprise entreprise);
 	public int ajouterDepartement(Departement dep);
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
 	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
-	
 }
